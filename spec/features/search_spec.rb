8f535bf5971b281ebbf01201d190dcb8b0006ca8
Fred Rondina <fredrondina96@gmail.com>
2020-11-14 19:44:15 -0700

    added passing tests for search bar functionality

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'When I visit the root page' do
   it 'I see a search bar where I can enter the title of a movie' do
     visit '/'
-    save_and_open_page
+    expect(page).to have_content("Welcome to Movie Search")
+    fill_in :search_title, with: "Vertigo"
+    click_on "Search Movies"
   end
 end
