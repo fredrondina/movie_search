439d0c86e11ecd596e7a5a12040f537e4931f265
Fred Rondina <fredrondina96@gmail.com>
2020-11-15 17:23:17 -0700

    added faraday api hit to movie db

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    # binding.pry
+    resp = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
+      req.params['api_key'] = ENV['movie_db_key']
+      req.params['language'] = 'en-US'
+      req.params['include_adult'] = false
+      req.params['query'] = params["search_title"]
+    end
+    results = JSON.parse(resp.body, symbolize_names: true)
+    binding.pry
   end
 end
