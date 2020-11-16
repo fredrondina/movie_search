8870de9cf097a1a886bb8cfbf3c57bf484961157
Fred Rondina <fredrondina96@gmail.com>
2020-11-15 17:53:08 -0700

    removed skip, all tests passing

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'When I visit the root page' do
   it 'I see a search bar where I can enter the title of a movie' do
-    skip
     visit '/'
     expect(page).to have_content("Welcome to Movie Search")
     fill_in :search_title, with: "Vertigo"
