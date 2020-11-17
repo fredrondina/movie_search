eb93347f37ad6c7ee7c0dab2339b4309d233feb3
Fred Rondina <fredrondina96@gmail.com>
2020-11-16 17:50:17 -0700

    created self.search method

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -6,11 +6,24 @@ class Movie
                 :overview
 
   def initialize(movie)
-    # binding.pry
     @id = movie[:id]
     @title = movie[:title]
     @release_date = movie[:release_date]
     @poster_url = movie[:poster_path]
     @overview = movie[:overview]
+  end
+
+  def self.search(search_title)
+    resp = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
+      req.params['api_key'] = ENV['movie_db_key']
+      req.params['language'] = 'en-US'
+      req.params['include_adult'] = false
+      req.params['query'] = search_title
+    end
+
+    results = JSON.parse(resp.body, symbolize_names: true)
+      @movies = results[:results].map do |result|
+      Movie.new(result)
+    end
   end
 end
