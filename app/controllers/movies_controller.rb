b523dd91009e582631bb7123184dbbc2464381e9
Fred Rondina <fredrondina96@gmail.com>
2020-11-16 17:50:03 -0700

    moved api call out of controller into movie poro class method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    resp = Faraday.get('https://api.themoviedb.org/3/search/movie') do |req|
-      req.params['api_key'] = ENV['movie_db_key']
-      req.params['language'] = 'en-US'
-      req.params['include_adult'] = false
-      req.params['query'] = params["search_title"]
-    end
-
-    results = JSON.parse(resp.body, symbolize_names: true)
-    @movies = results[:results].map do |result|
-      Movie.new(result)
-    end
+    @movies = Movie.search(params["search_title"])
   end
 end
