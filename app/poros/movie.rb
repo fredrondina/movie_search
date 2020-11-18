58fded67c85a198b042f9efda98a47271698efda
Fred Rondina <fredrondina96@gmail.com>
2020-11-17 17:41:12 -0700

    added get by id model method, new api hit

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -3,14 +3,17 @@ class Movie
                 :title,
                 :release_date,
                 :poster_url,
-                :overview
+                :overview,
+                :director
 
   def initialize(movie)
     @id = movie[:id]
     @title = movie[:title]
     @release_date = movie[:release_date]
     @poster_url = movie[:poster_path]
+    @backdrop_url = movie[:backdrop_path]
     @overview = movie[:overview]
+    @director = nil
   end
 
   def self.search(search_title)
@@ -18,5 +21,15 @@ class Movie
     @movies = results[:results].map do |result|
       Movie.new(result)
     end
+  end
+
+  def self.get_by_id(movie_id)
+    resp = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}") do |req|
+      req.params['api_key'] = ENV['movie_db_key']
+      req.params['language'] = 'en-US'
+      req.params['append_to_response'] = 'credits'
+    end
+    result = JSON.parse(resp.body, symbolize_names: true)
+    Movie.new(result)
   end
 end
