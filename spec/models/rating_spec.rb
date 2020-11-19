186dcda39cf31cd1d95b20788b8750132d7019d0
Fred Rondina <fredrondina96@gmail.com>
2020-11-18 20:19:21 -0700

    added movie id to model test for ratings

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Rating, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:movie_id) }
     it { should validate_presence_of(:thumbs_up) }
     it { should validate_presence_of(:thumbs_down) }
   end
