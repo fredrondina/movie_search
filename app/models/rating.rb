1593ccb1d698139a7c3ce04c430ba80c6c771323
Fred Rondina <fredrondina96@gmail.com>
2020-11-19 20:43:51 -0700

    added rated? model method for quick check of movie rating status

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,4 +5,9 @@ class Rating < ApplicationRecord
   validates :thumbs_up, presence: true
   validates :thumbs_down, presence: true
 
+  def rated?
+    return false if self.thumbs_up + self.thumbs_down == 0
+    true
+  end
+
 end
