d773699d2233f11105b5e667870d9b2773d5dc11
Fred Rondina <fredrondina96@gmail.com>
2020-11-19 20:43:13 -0700

    added loading of rating in the movies controller for show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,8 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.get_by_id(params[:movie_id])
+    @rating = Rating.find_or_create_by(movie_id: @movie.id) do |r|
+      r.title = @movie.title
+    end
   end
 end
