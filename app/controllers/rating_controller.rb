c0bfed097b8baba2276b7154e9bd9c45e50f1cd4
Fred Rondina <fredrondina96@gmail.com>
2020-11-22 19:12:03 -0700

    added rudimentary rating logic, needs refactoring

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -1,11 +1,18 @@
 class RatingController < ApplicationController
 
   def update
-    if params[:up_or_down] == 'up'
-      rating = Rating.find(params[:rating_id].to_i).increment!(:thumbs_up)
-    elsif params[:up_or_down] == 'down'
-      rating = Rating.find(params[:rating_id].to_i).increment!(:thumbs_down)
+    session[:rated] = [] if session[:rated].nil?
+    if session[:rated].include?(params[:movie_id].to_i)
+      flash.alert = "You've already rated this film, why not rate another?"
+      redirect_to "/movies/#{params[:movie_id]}"
+    else
+      if params[:up_or_down] == 'up'
+        rating = Rating.find_by('movie_id': params[:movie_id].to_i).increment!(:thumbs_up)
+      elsif params[:up_or_down] == 'down'
+        rating = Rating.find_by('movie_id': params[:movie_id].to_i).increment!(:thumbs_down)
+      end
+      session[:rated] << params[:movie_id].to_i
+      redirect_to "/movies/#{rating.movie_id}"
     end
-    redirect_to "/movies/#{rating.movie_id}"
   end
 end
