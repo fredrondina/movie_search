99a5e64b475c93839406af7c366f13d5e7bdc710
Fred Rondina <fredrondina96@gmail.com>
2020-11-22 19:12:52 -0700

    changed rating id to movie id param

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
   get '/movies/:movie_id', to: 'movies#show'
 
-  post '/rating/:rating_id/:up_or_down', to: 'rating#update'
+  post '/rating/:movie_id/:up_or_down', to: 'rating#update'
 
 end
